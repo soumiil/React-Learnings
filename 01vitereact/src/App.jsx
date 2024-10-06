869e792ab402fa03c18fd364499893f42cf27ff0
Soumil Agarwal <agarwalsoumil6@gmail.com>
2024-10-06 10:33:12 +0530

    new component

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,6 +1,11 @@
+import Chai from './Chai'
+
 function App() {
   return (
-    <h1>Chai aur React with Vite | Soumil Agarwal</h1>
+    <>
+      <Chai />
+      <h1>Hello</h1>
+    </>
   );
 }
 
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,8 @@
+// We made a component and then exported it
+function Chai() {
+    return (
+        <h3>Chai is ready!</h3>
+    )
+}
+
+export default Chai
