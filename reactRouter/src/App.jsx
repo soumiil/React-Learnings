b4b764c94f54e7ce5411770dc9a8218cb894889b
Soumil Agarwal <agarwalsoumil6@gmail.com>
2024-10-15 12:04:34 +0530

    project

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,12 @@
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+
+function App() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App;
